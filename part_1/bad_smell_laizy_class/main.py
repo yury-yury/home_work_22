55b23aba69ceb3df232e747a9fc22380d56d8c5a
Yury Yanovsky <118936006+yury-yury@users.noreply.github.com>
2023-02-01 11:12:09 +0300

    Add files via upload

---
+++ part_1/bad_smell_laizy_class/main.py
@@ -0,0 +1,37 @@
+# Один из классов не делает совсем ничего,
+# просто переадресует вызовы к другому классу.
+# Удалите этот класс и перенаправьте вызовы напрямую.
+
+class Unit:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def attack(self):
+        pass
+
+    def defense(self):
+        pass
+
+    def move(self, field):
+        field.set_unit(x=self.x, y=self.y, unit=self)
+
+
+class Field:
+    def set_unit(self, x, y, unit: Unit):
+        pass
+
+
+"""class FieldAdapter:
+    def __init__(self, field: Field):
+        self.field = field
+
+    def set_unit(self, x, y, unit: Unit):
+        self.field.set_unit(x, y, unit)"""
+
+
+class Main:
+    def __init__(self):
+        self.field = Field()
+        self.unit = Unit()
+        self.unit.move(self.field)
